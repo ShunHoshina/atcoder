f842a3e05cda2e064123b669708ba8e93d2a76c0
Shun Hoshina <s21c1105da@s.chibakoudai.jp>
2022-11-14 16:55:48 +0900

    create a.c

---
+++ abc/276/a.c
@@ -0,0 +1,15 @@
+/*======================================================================
+Project Name    : <abc276 a.c>
+File Name       : <a.c>
+Encoding        : <utf-8>
+Creation Date   : <2022>
+ 
+Copyright © <2022> <Shun Hoshina>. All rights reserved.
+ 
+This source code or any portion thereof must not be  
+reproduced or used in any manner whatsoever.
+======================================================================*/
+#include <stdio.h>
+int main(void){
+    return 0;
+}
